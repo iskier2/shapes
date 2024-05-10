5ba5c9834f5763799d76f0ab5e9e4f0c9d4bd564
Iskier2 <iskrafilip12@interia.pl>
2024-05-10 05:40:00 +0200

    shapes-2

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -2,10 +2,10 @@
 // Created by Iskra on 18.04.2024.
 //
 #include "shapes.hpp"
+#include <numeric>
 
 double calculate_total_area(const std::vector<Shape*>& shapes){
     double area = 0;
-    for(const auto& i : shapes)
-        area += i->area();
+    std::accumulate(shapes.begin(), shapes.end(), area, [](double acc, Shape* shape){return acc + shape->area();});
     return area;
 }
